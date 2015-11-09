97963098d9d09d7b590a5d51efd9bc854f7f79d2
Nik Nyby <nnyby@columbia.edu>
2015-11-09 14:45:31 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -2661,7 +2661,6 @@ SherdBookmarklet = {
         this.displayAsset = function(asset,index) {
             var assetUrl = asset.sources[asset.primary_type];
             if(assetUrl !== undefined){
-                // make sure to strip out any url params
                 asset.sources[asset.primary_type] = assetUrl;
             }
             if (!asset) return;
